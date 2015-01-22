dce02ce9dc724ad1576ec3ca22e4307d3dfaaf1e
Miles Z. Sterrett <miles@mileszs.com>
2015-01-22 09:12:14 -0500

    Seemed like it needed another newline

--- runthis.rb
+++ runthis.rb
@@ -1,3 +1,3 @@
 require_relative 'thought_verbs'
-puts "\n********************************************\n"
+puts "\n********************************************\n\n"
 require_relative 'said_bookisms'
